4f1f82491bc4b4ad3aa8b553b7df6f1c335fe13d
Laurens Schaap <l.schaap91@gmail.com>
2023-05-09 14:58:27 +0200

    Huiswerk

--- les3/hospital.py
+++ les3/hospital.py
@@ -15,18 +15,21 @@ reg_email = StringVar()
 reg_password = StringVar()
 
 def check_email():
-    if email == retype_email:
+    if reg_email.get() == retype_email.get():
         return True
+    elif '@' not in reg_email.get():
+        messagebox.showerror(title='Error', message='Not a valid E-mail.')
     else:
         messagebox.showerror(title="Error", message='E-mail is not the same.')
         return False
+
 
 # Registration Window
 def register_window():
     window = Toplevel(root)
     register_frame = Frame(window,pady=25)
     window.title('Register')
-    window.geometry('300x300')
+    window.geometry('300x150')
     
     
     email_label = Label(register_frame, text='E-Mail').grid(column=0, row=0)
@@ -36,11 +39,15 @@ def register_window():
     password_label = Label(register_frame, text='Password').grid(column=0, row=2)
     password_entry = Entry(register_frame, textvariable=reg_password, show='*').grid(column=1, row=2, columnspan=2)
     
-    register_btn = Button(register_frame, text='Register').grid(column=1, row=3, pady=10)
-    cancel_btn = Button(register_frame, text="Cancel").grid(column=2, row=3, pady=10)
+    register_btn = Button(register_frame, text='Register', command=register_user).grid(column=1, row=3, pady=10)
+    cancel_btn = Button(register_frame, text="Cancel", command=window.destroy).grid(column=2, row=3, pady=10)
     
     register_frame.pack()
     
+def register_user():
+    with open('user.txt', 'a') as f:
+        f.write(reg_email.get() + ", " + reg_password.get() + ";\n")
+        f.close()
 
 
 # Login widgets
@@ -52,7 +59,7 @@ password_entry = Entry(frame, textvariable=log_password, show='*').grid(column=1
 # Buttons
 login_btn = Button(frame, text='Login').grid(column=0, row=2,pady=10)
 register_btn = Button(frame, text='Register', command=register_window).grid(column=1, row=2,pady=10)
-cancel_btn = Button(frame, text='Cancel').grid(column=2, row=2,pady=10)
+cancel_btn = Button(frame, text='Cancel', command=root.destroy).grid(column=2, row=2,pady=10)
 
 
 
--- les3/hospital_system.py
+++ les3/hospital_system.py
@@ -11,6 +11,12 @@ ACTIVE_USER = []
 
 main_frame = Frame(root).pack()
 
+# Variabelen
+login_email_string = StringVar()
+login_password_string = StringVar()
+register_email = StringVar()
+register_retype_email = StringVar()
+register_password = StringVar()
 
 
 # Login page
@@ -21,21 +27,16 @@ def login_page():
     
     login_frame = Frame(login_window)
 
-    # Variables for names
-    email = StringVar()
-    password = StringVar()
-
-
     # Entry Fields
     label_email = Label(login_frame, text='E-Mail: ').grid(column=0, row=0, pady=5)
-    entry_email = Entry(login_frame, textvariable=email).grid(column=1, row=0,pady=5)
+    entry_email = Entry(login_frame, textvariable=login_email_string).grid(column=1, row=0,pady=5)
     label_password = Label(login_frame, text='Password: ').grid(column=0, row=1, pady=5)
-    entry_password = Entry(login_frame, show="*", textvariable=password).grid(column=1, row=1, pady=5)
+    entry_password = Entry(login_frame, show="*", textvariable=login_password_string).grid(column=1, row=1, pady=5)
 
     # Buttons
     btn_login = Button(login_frame, text="Login").grid(column=0, row=2)
     btn_register = Button(login_frame, text="Register", command=register_form).grid(column=1, row=2)
-    btn_cancel = Button(login_frame, text="Cancel").grid(column=2, row=2)
+    btn_cancel = Button(login_frame, text="Cancel", command=login_window.destroy).grid(column=2, row=2)
     
     login_frame.pack()
 
@@ -43,39 +44,34 @@ def login_page():
 def register_form():
     register_window = Toplevel(root)
     register_window.geometry('400x250')
-    
     register_frame = Frame(register_window)
     
-    email = StringVar()
-    retype_email = StringVar()
-    password = StringVar()
-
-
     # Entry Fields
     label_email = Label(register_frame, text='E-Mail: ').grid(column=0, row=0, pady=5)
-    entry_email = Entry(register_frame, textvariable=email).grid(column=1, row=0,pady=5)
+    entry_email = Entry(register_frame, textvariable=register_email).grid(column=1, row=0,pady=5)
     label_retype_email = Label(register_frame, text='Retype E-Mail: ').grid(column=0, row=1, pady=5)
-    entry_retype_email = Entry(register_frame, textvariable=retype_email).grid(column=1, row=1, pady=5)
+    entry_retype_email = Entry(register_frame, textvariable=register_retype_email, validate='focusout', validatecommand=check_email).grid(column=1, row=1, pady=5)
     label_password = Label(register_frame, text='Password: ').grid(column=0, row=2, pady=5)
-    entry_password = Entry(register_frame, show="*", textvariable=password).grid(column=1, row=2, pady=5)
+    entry_password = Entry(register_frame, show="*", textvariable=register_password).grid(column=1, row=2, pady=5)
 
     # Buttons
-    btn_register = Button(register_frame, text="Register", command=check_registration).grid(column=0, row=3)
-    btn_cancel = Button(register_frame, text="Cancel").grid(column=1, row=3)
+    btn_register = Button(register_frame, text="Register").grid(column=0, row=3)
+    btn_cancel = Button(register_frame, text="Cancel", command=register_window.destroy).grid(column=1, row=3)
 
+    # Pack
     register_frame.pack()
     
     
+def check_email():
+    if '@' not in register_email.get():
+        messagebox.showerror('Error', 'Dit is geen geldig e-mail adres.')
+        return False
+    elif register_email.get() != register_retype_email.get():
+        messagebox.showerror('Error', 'E-mail adressen komen niet overeen.')
+        return False
+    elif register_email.get() == register_retype_email.get():
+        return True
     
-    
-def check_registration():
-    print(email)
-    print(retype_email)
-
-
-
-        
-
 if LOGGED_IN == False:
     main_label = Label(main_frame, text='Je bent nog niet ingelogd. Klik hieronder om in te loggen').pack()
     login_btn = Button(main_frame, text='Login', command=login_page).pack()
